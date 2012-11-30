1152ee5bca390c95ddb323d6b8d77191c68f1316
Heather H <hornehm@muohio.edu>
2012-11-30 01:43:19 -0500

    Added some code from class
    I forgot to save before I committed last time. Here is the code from
    class.

--- vc10/ListGraph.cpp
+++ vc10/ListGraph.cpp
@@ -1,15 +1,29 @@
 #include "ListGraph.h"
 
+//Constructor from CSE 274
 ListGraph::ListGraph(int numNodes){
+	edgeList.resize(numNodes);
+	//numNodes= 0;
 }
 
 void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
 }
 
+//Code from CSE 274
 EdgeWeight ListGraph::weight(NodeID u, NodeID v) const{
+	edgeList[u];
+	EList::const_iterator it;
+	for(it = edgeList[u].begin(); it!=edgeList[u].end(); it++){
+		NWPair theEdge = *it;
+		if(theEdge.first == v){
+			return theEdge.second;
+		}
+	}
 }
 
+//Code from CSE 274
 std::list<NWPair> ListGraph::getAdj(NodeID u) const{
+	return edgeList[u];
 }
 
 unsigned ListGraph::degree(NodeID u) const{
